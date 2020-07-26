38013e047d6c08d3cb75333362489e912bc16836
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-07-26 11:33:22 -0400

    Updated seeds and added additional shell commands

---
+++ seeds/seeds.sql
@@ -0,0 +1,246 @@
+INSERT INTO
+  public.activity (description, positive, fund_amt)
+VALUES
+  ('Took longer to wake up', TRUE, 10000000),
+  ('Test 1', TRUE, 2000000),
+  ('Test 2', false, 2000000),
+  ('A Test!', TRUE, 10000000),
+  ('New Spanish Book', TRUE, 50000000),
+  ('Read books', TRUE, 5000000),
+  ('Lazy', false, 100000000),
+  ('New Item', TRUE, 5000000),
+  ('Pomodoro', TRUE, 10000000),
+  ('Missed Activity with Kids', false, 10000000);
+
+INSERT INTO
+  public.activity (description, positive, fund_amt)
+VALUES
+  ('Did not workout', false, 10000000),
+  ('Did not practice music', false, 10000000),
+  (
+    'Did not make career improvement activity',
+    false,
+    10000000
+  ),
+  ('Wasting time', false, 100000000),
+  ('Missed Pomodoro Goals', false, 10000000),
+  ('Did not stop after pomodoro', false, 5000000),
+  ('Drink 1L water', TRUE, 5000000),
+  ('Pomodoro', TRUE, 5000000),
+  ('abc', TRUE, 33000000),
+  ('pangarap', TRUE, 22000000);
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-05 00:55:43.455'),
+  (2, '2020-03-05 01:00:03.099'),
+  (3, '2020-03-05 22:39:59.026'),
+  (4, '2020-03-05 22:47:36.194'),
+  (5, '2020-03-05 22:47:41.095'),
+  (6, '2020-03-05 23:12:07.126'),
+  (7, '2020-03-05 23:27:52.902'),
+  (8, '2020-03-05 23:27:58.941'),
+  (9, '2020-03-06 22:33:36.521'),
+  (10, '2020-03-06 22:34:13.643');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-09 23:01:11.533'),
+  (2, '2020-03-11 01:12:41.804'),
+  (3, '2020-03-14 00:00:00.000'),
+  (4, '2020-03-13 23:00:00.000'),
+  (5, '2020-03-14 18:22:37.810'),
+  (6, '2020-03-14 18:38:22.387'),
+  (7, '2020-03-14 18:54:41.411'),
+  (8, '2020-03-14 19:05:03.419'),
+  (9, '2020-03-14 19:05:14.976'),
+  (10, '2020-03-14 19:05:30.740');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-14 19:05:38.579'),
+  (2, '2020-03-14 19:12:50.561'),
+  (3, '2020-03-14 19:13:59.799'),
+  (4, '2020-03-15 20:11:07.704'),
+  (5, '2020-03-15 20:11:10.445'),
+  (6, '2020-03-15 20:11:12.956'),
+  (7, '2020-03-15 20:11:13.524'),
+  (8, '2020-03-16 02:10:06.980'),
+  (9, '2020-03-16 02:10:43.223'),
+  (10, '2020-03-16 02:36:35.152');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-16 22:31:58.610'),
+  (2, '2020-03-16 22:39:09.458'),
+  (3, '2020-03-16 22:45:51.495'),
+  (4, '2020-03-16 22:49:14.290'),
+  (5, '2020-03-16 02:28:57.000'),
+  (6, '2020-03-18 21:27:32.378'),
+  (7, '2020-03-18 21:32:34.578'),
+  (8, '2020-03-18 21:32:36.481'),
+  (9, '2020-03-18 21:32:38.081'),
+  (10, '2020-03-18 21:32:39.478');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-18 21:32:40.924'),
+  (2, '2020-03-18 21:32:43.439'),
+  (3, '2020-03-18 21:32:44.961'),
+  (4, '2020-03-18 21:32:46.462'),
+  (5, '2020-03-18 21:33:02.419'),
+  (6, '2020-03-19 09:54:42.815'),
+  (7, '2020-03-19 12:31:59.011'),
+  (8, '2020-03-19 13:59:25.217'),
+  (9, '2020-03-19 13:59:28.420'),
+  (10, '2020-03-19 13:59:40.982');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-19 13:59:43.640'),
+  (2, '2020-03-19 17:55:14.530'),
+  (3, '2020-03-19 17:56:15.772'),
+  (4, '2020-03-19 17:56:51.530'),
+  (5, '2020-03-19 17:56:53.533'),
+  (6, '2020-03-19 17:56:55.293'),
+  (7, '2020-03-19 22:58:00.454'),
+  (8, '2020-03-19 22:58:04.596'),
+  (9, '2020-03-20 22:27:45.646'),
+  (10, '2020-03-20 22:27:46.289');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-20 22:27:49.667'),
+  (2, '2020-03-20 22:27:51.966'),
+  (3, '2020-03-20 22:27:53.489'),
+  (4, '2020-03-20 22:27:55.169'),
+  (5, '2020-03-20 22:27:56.671'),
+  (6, '2020-03-20 22:27:58.227'),
+  (7, '2020-03-20 22:27:59.926'),
+  (8, '2020-03-20 22:28:01.831'),
+  (9, '2020-03-20 22:28:08.247'),
+  (10, '2020-03-20 22:28:15.749');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (1, '2020-03-20 22:59:56.098'),
+  (2, '2020-03-20 23:02:02.479'),
+  (3, '2020-03-21 22:31:09.508'),
+  (4, '2020-03-21 22:31:11.847'),
+  (5, '2020-03-21 23:29:08.147'),
+  (6, '2020-03-21 23:29:49.926'),
+  (7, '2020-03-21 23:30:30.185'),
+  (8, '2020-03-21 23:30:44.932'),
+  (9, '2020-03-21 23:30:45.750'),
+  (10, '2020-03-21 23:30:46.507');
+
+INSERT INTO
+  public.activityaction (activity_id, action_timestamp)
+VALUES
+  (4, '2020-03-21 23:31:44.226'),
+  (2, '2020-03-21 23:31:46.467'),
+  (8, '2020-03-21 23:32:03.586'),
+  (9, '2020-03-21 23:31:44.226'),
+  (3, '2020-03-21 23:31:46.467'),
+  (9, '2020-03-21 23:32:03.586');
+
+INSERT INTO
+  public.wishitem (description, price, "source", priority, STATUS)
+VALUES
+  ('Another new', 33, 'dfsdfsf', 'MEDIUM', 'bought'),
+  ('Raizza', 323, 'Here man', 'LOW', 'disabled'),
+  ('ajedrez', 334, 'Aldrin', 'MEDIUM', 'bought'),
+  (
+    'new udemy course',
+    15,
+    'udemy.com',
+    'MEDIUM',
+    'disabled'
+  ),
+  (
+    'New Book',
+    1000000,
+    'www.amazon.ca',
+    'VERY_LOW',
+    'bought'
+  ),
+  (
+    'New watch band',
+    10000000,
+    'NULL',
+    'VERY_HIGH',
+    'bought'
+  ),
+  (
+    'New watch band',
+    10000000,
+    'NULL',
+    'VERY_HIGH',
+    'bought'
+  ),
+  (
+    'New watch band',
+    10000000,
+    NULL,
+    'VERY_HIGH',
+    'bought'
+  ),
+  (
+    'New watch band',
+    10000000,
+    'NULL',
+    'VERY_HIGH',
+    'bought'
+  ),
+  (
+    'book11',
+    11000000,
+    'Trenta',
+    'VERY_HIGH',
+    'bought'
+  );
+
+INSERT INTO
+  public.wishitem (description, price, "source", priority, STATUS)
+VALUES
+  ('New', 22, 'Here', 'VERY_LOW', 'bought'),
+  ('book3', 34, 'NULL', 'VERY_HIGH', 'bought'),
+  ('book2', 2, 'NULL', 'VERY_HIGH', 'bought'),
+  ('refresh now', 33, 'NULL', 'VERY_HIGH', 'bought'),
+  ('book99', 990, 'NULL', 'VERY_HIGH', 'bought'),
+  (
+    'New book for spanish learning',
+    344000000,
+    'NULL',
+    'VERY_HIGH',
+    'not_bought'
+  ),
+  (
+    'Madden 20',
+    80000000,
+    'NULL',
+    'VERY_HIGH',
+    'not_bought'
+  ),
+  (
+    'Teddy bear',
+    10000000,
+    'NULL',
+    'VERY_HIGH',
+    'bought'
+  ),
+  (
+    'book114',
+    11400000,
+    'Trenta',
+    'VERY_HIGH',
+    'bought'
+  );
