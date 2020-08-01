933cdcf8c17ae615a862b4b69598b2e2a637c2fc
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2020-08-01 14:04:51 -0400

    Added action loader

--- dataloaders/LoaderMiddleware.go
+++ dataloaders/LoaderMiddleware.go
@@ -11,6 +11,7 @@ import (
 )
 
 const activityLoaderKey = "activityLoader"
+const actionLoaderKey = "actionLoader"
 
 func LoaderMiddleware(db *pg.DB, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +47,40 @@ func LoaderMiddleware(db *pg.DB, next http.Handler) http.Handler {
 			wait:     1 * time.Millisecond,
 		}
 
+		actionLoader := &ActionLoader{
+			fetch: func(ids []string) ([]*model.Action, []error) {
+				var activities []*model.Action
+
+				err := db.Model(&activities).Where("id in (?)", pg.In(ids)).Select()
+
+				if err != nil {
+					fmt.Println("Error in ActionLoaderMiddleware", err)
+				}
+
+				// Create buffer
+				activitiesBuff := make(map[string]*model.Action)
+
+				// Populate buffer
+				for _, action := range activities {
+					activitiesBuff[action.ID] = action
+				}
+
+				// Make sorted result slice
+				results := make([]*model.Action, len(ids))
+
+				// Map buffer to result
+				for i, id := range ids {
+					results[i] = activitiesBuff[id]
+				}
+
+				return results, nil
+			},
+			maxBatch: 100,
+			wait:     1 * time.Millisecond,
+		}
+
 		ctx := context.WithValue(r.Context(), activityLoaderKey, activityLoader)
+		ctx = context.WithValue(ctx, actionLoaderKey, actionLoader)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
@@ -54,4 +88,8 @@ func LoaderMiddleware(db *pg.DB, next http.Handler) http.Handler {
 
 func GetActivityLoader(ctx context.Context) *ActivityLoader {
 	return ctx.Value(activityLoaderKey).(*ActivityLoader)
+}
+
+func GetActionLoader(ctx context.Context) *ActionLoader {
+	return ctx.Value(actionLoaderKey).(*ActionLoader)
 }
